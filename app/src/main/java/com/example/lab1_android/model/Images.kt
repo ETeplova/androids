f392662ec265ae6d674e070744dd315054d7f63f
Елена Теплова <teplova@MacBook-Air-Elena.local>
2021-10-10 15:49:30 +0600

    First task done

---
+++ app/src/main/java/com/example/lab1_android/model/Images.kt
@@ -0,0 +1,7 @@
+package com.example.lab1_android.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Images (@DrawableRes val imageResourceId: Int,
+                   @StringRes val stringResourceId: Int)
